199e30a9169a7f55679a56c9b6ea1532bc5db19d
lance jared <lance21@gmail.com>
2020-02-09 21:08:33 +0800

    electronicsUI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateDeviceComponent } from './create-device/create-device.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateDeviceComponent
   ],
   imports: [
     BrowserModule,
