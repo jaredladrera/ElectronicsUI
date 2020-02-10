8d65b65c33df967ddad7128f4b436e4c0b7d9f9d
lance <ladrera21@gmail.com>
2020-02-10 17:26:30 +0800

    forms creating models electronicsDetails

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateDeviceComponent } from './create-device/create-device.component';
+import { from } from 'rxjs';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'create', component: CreateDeviceComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
